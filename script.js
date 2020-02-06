561bf8e08272b4dabbab6e9b31e64c848b914237
Gabe Young <gabriel_young@emerson.edu>
2020-02-06 14:16:59 -0500

    real JS this time

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 var APIkey= "668b9a941fa9096a988265ad658e599c"
-var queryURL ="api.openweathermap.org/data/2.5/weather?q="
-
+var queryURL ="http://api.openweathermap.org/data/2.5/weather?q="
+ 
 
 function search(){
     console.log(queryURL);
     var searchQuery = $("#field").val();
     console.log(searchQuery);
     $.ajax({
-        url: queryURL +searchQuery+APIkey,
+        url: queryURL + searchQuery + "&appid=" + APIkey,
         method: "GET",
         dataType:"jsonp",
     }).then(function(response){
         console.log(response);
-    })
-}
+        $("#city").html("<h2>" + response.name + "<h2>");
+        var tempF = (response.main.temp - 273.15)*1.80 + 32;
+        $("#temp").html("<h3>" + "Temperature: " + tempF + "<h3>");
+        })
+    }
+
+
+
