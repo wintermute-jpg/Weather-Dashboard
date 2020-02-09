860b652eec9f193ccdef2fd9a7419de5af326881
Gabe Young <gabriel_young@emerson.edu>
2020-02-08 19:20:23 -0500

    Icons + Search History

--- script.js
+++ script.js
@@ -1,22 +1,95 @@
 var APIkey= "668b9a941fa9096a988265ad658e599c"
-var queryURL ="http://api.openweathermap.org/data/2.5/weather?q="
- 
+var weatherURL ="http://api.openweathermap.org/data/2.5/weather?q="
+var date = moment().format("MMM Do YY");
+var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q="
 
-function search(){
-    console.log(queryURL);
+
+window.onload = function(){
+ $("#searchBtn").click(function(){
+    //console.log(weatherURL);
     var searchQuery = $("#field").val();
-    console.log(searchQuery);
+    $("#history").append("<li>"+ searchQuery+ "</li>");
+       
     $.ajax({
-        url: queryURL + searchQuery + "&appid=" + APIkey,
+        url: weatherURL + searchQuery + ",us&mode"+"&appid=" + APIkey,
         method: "GET",
         dataType:"jsonp",
     }).then(function(response){
         console.log(response);
-        $("#city").html("<h2>" + response.name + "<h2>");
-        var tempF = (response.main.temp - 273.15)*1.80 + 32;
-        $("#temp").html("<h3>" + "Temperature: " + tempF + "<h3>");
+        var temp= (response.main.temp - 273.15)*1.80 + 32;
+        var tempF = temp.toFixed(2);
+        var icon = response.weather[0].icon ;
+        $("#city").html("<h2>" + response.name + "(" + date + ")"+ "<img src='http://openweathermap.org/img/wn/"+icon+"@2x.png'"+"<h2>");
+       console.log(icon);
+        $("#temp").html("<p>" + "Temperature: " + tempF + "F"+"<p>");
+        $("#humidity").html("<p>" + "Humidity: " + response.main.humidity);
+        $("#wind").html("<p>" + "Wind Speed: " + response.wind.speed);
+        $.ajax({
+            url: forecastURL + searchQuery + "&appid=" + APIkey,
+            method:"GET",
+            dataType:"jsonp",
+        }).then(function(response){
+            console.log(response);
+            var n = 0
+            //create a loop that fills in thex info 
+            for(var i=0; i<=5; i++) {
+            //for loop that increases var i=0 by one till its equal to five.
+            //var n in loop to represent index of date
+            var find = response.list[n];
+            var foreDate= find.dt_txt
+            var foreIcon= find.weather[0].icon;
+            //var date = find.dt_txt;
+          //  console.log(date);
+            console.log(foreIcon);
+            var forTemp = ((find.main.temp - 273.15)*1.80 +32).toFixed(2);
+            //console.log(forTemp);
+            $("#day"+ i).html("<p>"+ foreDate + "<p>"+"<img src='http://openweathermap.org/img/wn/"+foreIcon+"@2x.png' style='width:50px;height:50px;'>"+ "<p>" + "Tempurature F" +forTemp + "<p>"+"<p>" +"Humidity: " + find.main.humidity + "<p>");
+            n ++;
+            n ++;
+            n ++;
+            n ++;
+            n ++;
+            n ++;
+            n ++;
+            n ++;
+            //increases n by 8s
+            //iterates
+            console.log(n);
+            //console.log(i);
+            }
         })
+        })
+    function history(){
+        $("#history").append("<li>"+ searchQuery+ "</li>");
+        var searchHist = $("#history").innerText;
+        console.log(searchHist);
+        //localStorage.setItem('preSearch', JSON.stringify(searchHist));
+        
     }
+    
+    }
+)
+}
+
+
+    //history function 
+//store user input in local storage
+// on page load, fill in the history bar with stored data
+//function store(item_id){
+    //get field input and set as var
+    //console.log(input);
+    //post it to local storage
+   // localStorage.setItem("search", JSON.stringify(searchQuery));
+    //console.log(localStorage);   
+//}
+
+//create forecast function
+//basically the same as search function 
+//AJAX call with specified syntax to provide forecast data
+//Slot the data into divs inside forecast
+//include date, temp and humidity
+
+//impliment icons for each kind of weather event
 
 
 
